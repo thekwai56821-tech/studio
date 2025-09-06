2371bf4c7b4a236be986a72cf62886b402cb7062
thek wai <thekwai56821@gmail.com>
2025-09-06 16:42:33 +0000

    Animation ကို မှိန်ရာမှ တဖြေးဖြေးအရောင်တောက်လာသည့် Animation ထားပါ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,8 +89,8 @@ export default {
           },
         },
         'slide-in': {
-          '0%': { transform: 'translateY(20px)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
         },
         'marquee': {
           '0%': { transform: 'translateX(100%)' },
