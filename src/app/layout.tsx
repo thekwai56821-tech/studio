ddecce6cead80b6122d04168190532b9ae1abaff
thek wai <thekwai56821@gmail.com>
2025-09-06 16:32:33 +0000

    ဘေးဘောင်ပတ်ပတ်လည်ကို အပြာရောင် ဖြင့် ပတ်လည် ထားပါ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased border-4 border-blue-800">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
