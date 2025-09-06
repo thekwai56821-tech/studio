7e44007a10ab0c91cf6f77e1b7048a33032956f2
thek wai <thekwai56821@gmail.com>
2025-09-06 16:09:37 +0000

    Background color အဝါ ၊အစိမ်း ၊ အနီ ရောင် အချိုးညီဖြည့်ပါ

--- src/components/icons/shield-icon.tsx
+++ src/components/icons/shield-icon.tsx
@@ -2,23 +2,12 @@ import * as React from 'react'
 
 const ShieldIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 120" {...props}>
-      <path
-        d="M50 0L95 20V60C95 90 65 115 50 120C35 115 5 90 5 60V20L50 0Z"
-        fill="#00008B"
-      />
-      <path
-        d="M50 4L91 22.4V59.4C91 86.4 63.4 109 50 113.4C36.6 109 9 86.4 9 59.4V22.4L50 4Z"
-        fill="none"
-        strokeWidth="2"
-      >
-        <animate attributeName="d" dur="0s" to="M50 4L91 22.4V59.4C91 86.4 63.4 109 50 113.4C36.6 109 9 86.4 9 59.4V22.4L50 4Z" fill="freeze" />
-      </path>
       <defs>
-        <clipPath id="shield-clip-inner">
-          <path d="M50 4L91 22.4V59.4C91 86.4 63.4 109 50 113.4C36.6 109 9 86.4 9 59.4V22.4L50 4Z" />
+        <clipPath id="shield-clip">
+          <path d="M50 0L95 20V60C95 90 65 115 50 120C35 115 5 90 5 60V20L50 0Z" />
         </clipPath>
       </defs>
-      <g clipPath="url(#shield-clip-inner)">
+      <g clipPath="url(#shield-clip)">
         <rect width="100" height="40" y="0" fill="#FECB00" />
         <rect width="100" height="40" y="40" fill="#34B233" />
         <rect width="100" height="40" y="80" fill="#EA2839" />
@@ -27,6 +16,12 @@ const ShieldIcon = (props: React.SVGProps<SVGSVGElement>) => (
           fill="white"
         />
       </g>
+      <path
+        d="M50 0L95 20V60C95 90 65 115 50 120C35 115 5 90 5 60V20L50 0Z"
+        fill="none"
+        stroke="hsl(var(--border))"
+        strokeWidth="4"
+      />
     </svg>
   );
   
